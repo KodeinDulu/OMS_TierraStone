bca9eca5d492b90512849d443dcfffa732d99492
VncntRnld <vincent.ronald@ti.ukdw.ac.id>
2026-05-17 20:08:37 +0700

    [Revisi] Clear admin widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -53,7 +53,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\Filament\Admin\Widgets')
             ->widgets([
-                OrderSummaryTable::class,
+                AccountWidget::class,
                 \App\Filament\Sales\Widgets\SalesDashboard::class,
             ])
             ->middleware([
